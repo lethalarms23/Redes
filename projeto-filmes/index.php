de2dc8a0d1f24bfec3259b85583eb73283ef09fc
lethalarms <hiddankill@gmail.com>
2021-01-06 10:20:06 +0000

    sistema CRUD - read

--- projeto-filmes/index.php
+++ projeto-filmes/index.php
@@ -19,7 +19,7 @@
         $stm->execute();
         $res=$stm->get_result();
         while($resultado = $res->fetch_assoc()){
-            echo '<a href="filmes_show.php?filme='.$resultado['id_filme'].">";
+            echo '<a href="filmes_show.php?filme='.$resultado['id_filme'].'">';
             echo $resultado['titulo'];
             echo '</a>';
             echo '<br>';
